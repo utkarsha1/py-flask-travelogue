9d0deff14b2a94715deb7e5812522c5bbdf1a013
Utkarsha Padhye <utkarsha2@hotmail.com>
2016-04-20 21:29:26 -0700

    - Complete GUI openID login elements

--- flask/bin/activate.fish
+++ flask/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelavent variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/utkarsha/py-flask-app/microblog/flask"
+set -gx VIRTUAL_ENV "/Users/utkarsha/py-flask-app/travelogue/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
